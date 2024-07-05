3d7a2d31122bc1da75ec55ec8054fde118c51465
Matthew Holandez <91989998+matthewholandez@users.noreply.github.com>
2024-07-05 14:34:54 -0400

    first

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+FOLLOWERS = 'followers_1.json'
+FOLLOWING = 'following.json'
+
+parsed_followers = []
+parsed_following = []
+
+with open(FOLLOWERS) as ers:
+    loaded_followers = json.load(ers)
+    for follower in loaded_followers:
+        parsed_followers.append(follower["string_list_data"][0]["value"])
+
+with open(FOLLOWING) as ing:
+    loaded_following = json.load(ing)
+    for followed in loaded_following["relationships_following"]:
+        followed_username = followed["string_list_data"][0]["value"]
+        if followed_username not in parsed_followers:
+            print('{} does not follow you back!'.format(followed_username))
